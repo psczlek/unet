0100e1cf662db060c4d70cc164e80a8c285a06dc
psczlek <cyprian_ccp@proton.me>
2024-10-08 00:48:07 +0200

    add blank lines

--- src/unet/__main__.py
+++ src/unet/__main__.py
@@ -14,11 +14,14 @@ def unet_entry() -> None:
     if current_platform not in supported_platforms:
         from unet.printing import eprint
         eprint(f"unet has not been tested on '{current_platform}'")
+
     python_version = sys.version.split()[0]
     if sys.version_info < (3, 12):
         from unet.printing import eprint
         eprint(f"unet requires python 3.12+, used version {python_version}")
+
     sys.path.append(str(Path().parent.absolute()))
+
     from unet.main import main
     main(sys.argv[1:])
 
