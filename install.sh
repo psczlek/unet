1fbe03615d728772039f337cfa3ca28590d8ca0e
Fedor Alekseyev <57331505+theosfa@users.noreply.github.com>
2025-03-22 18:50:24 +0100

    Update and rename install-app.sh to install.sh
    Fixed colors

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 print_progress() {
     local step=$1
     local total_steps=$2
     local percent=$(( (step * 100) / total_steps ))
-    printf "\r\033[92m[%3d%%]\033[0m \033[93m%d/%d %s\033[0m\n" "$percent" "$step" "$total_steps" "$3"
+    printf "\r\x1b[0;92m[%3d%%]\x1b[0m \x1b[0;93m%d/%d %s\x1b[0m\n" "$percent" "$step" "$total_steps" "$3"
 }
 
 
@@ -33,69 +33,66 @@ while true; do
     -d|--dev)  DEV=1; TIMES=$(($TIMES+1)) ;;
     -i|--install)  INSTALL=1 ;;
     --)        shift ; break ;;
-    *)         echo "\033[91merror:\033[0m unknown option: $1" ; return ;;
+    *)         echo "\x1b[0;91merror:\x1b[0m unknown option: $1" ; return ;;
   esac
   shift
 done
 
 if [ $# -ne 0 ]; then
-  echo "\033[91merror:\033[0m unknown option(s): $@"
+  echo "\x1b[0;91merror:\x1b[0m unknown option(s): $@"
   return
 fi
 
 if [ $HELP -eq 1 ]; then
-    echo '\033[94m-h,  --help\033[0m\tsee this message'
-    echo '\033[94m-v,  --virtual\033[0m\tuse virtual environment(default: global)'
-    echo '\033[94m-d,  --dev\033[0m\tuse developing tools'
-    echo '\033[94m-i,  --install\033[0m\tinstall app'
-    return
-fi
-
-
-
-#
-# Test for venv
-#
-print_progress $ITER $TIMES "Checking for venv module"
-ITER=$(($ITER+1))
-python3 -m venv -h > /dev/null
-if [ $? -ne 0 ]; then
-    printf -e "\n\033[91merror:\033[0m python3 -m venv: venv module not installed. To proceed with installation please install the venv module\n"
-    return
-fi
-
-#
-# Test for pip
-#
-print_progress $ITER $TIMES "Checking for pip module"
-ITER=$(($ITER+1))
-python3 -m pip -h > /dev/null
-if [ $? -ne 0 ]; then
-    printf -e "\n\033[91merror:\033[0m python3 -m pip: pip module not installed. To proceed with installation please install the pip module\n"
+    echo '\x1b[1;94m-h,  --help\x1b[0m\tsee this message'
+    echo '\x1b[1;94m-v,  --virtual\x1b[0m\tuse virtual environment(default: global)'
+    echo '\x1b[1;94m-d,  --dev\x1b[0m\tuse developing tools'
+    echo '\x1b[1;94m-i,  --install\x1b[0m\tinstall app'
     return
 fi
 
 if [ $INSTALL -eq 1 ]; then
+    #
+    # Test for pip
+    #
+    print_progress $ITER $TIMES "Checking for pip module"
+    ITER=$(($ITER+1))
+    python3 -m pip -h > /dev/null
+    if [ $? -ne 0 ]; then
+        printf -e "\n\x1b[0;91merror:\x1b[0m python3 -m pip: pip module not installed. To proceed with installation please install the pip module\n"
+        return
+    fi
     if [ $VIRTUAL -eq 1 ]; then
+        #
+        # Test for venv
+        #
+        print_progress $ITER $TIMES "Checking for venv module"
+        ITER=$(($ITER+1))
+        python3 -m venv -h > /dev/null
+        if [ $? -ne 0 ]; then
+            printf -e "\n\x1b[0;91merror:\x1b[0m python3 -m venv: venv module not installed. To proceed with installation please install the venv module\n"
+            return
+        fi
+        
         print_progress $ITER $TIMES "Creating virtual environment"
         ITER=$(($ITER+1))
         # create virtual environment
         python3 -m venv ~/.virtualenv/unet
         # activate virtual environment
         source ~/.virtualenv/unet/bin/activate
-        echo '\n\033[93mNOTE:\033[0m \033[96mDedicated virtual environment has been created at:\033[0m \033[92m~/.virtualenv/unet\033[0m\n'
+        echo '\n\x1b[0;93mNOTE:\x1b[0m \x1b[0;96mDedicated virtual environment has been created at:\x1b[0m \x1b[0;92m~/.virtualenv/unet\x1b[0m\n'
     fi
     print_progress $ITER $TIMES "Installing all dependencies"
     ITER=$(($ITER+1))
     # isntall all dependencies
     python3 -m pip install -r requirements.txt
-    echo '\n\033[93mNOTE:\033[0m \033[96mAll app dependencies has been installed\033[0m\n'
+    echo '\n\x1b[0;93mNOTE:\x1b[0m \x1b[0;96mAll app dependencies has been installed\x1b[0m\n'
     if [ $DEV -eq 1 ]; then
       print_progress $ITER $TIMES "Installing dev dependencies"
       ITER=$(($ITER+1))
       # isntall dev dependencies
       python3 -m pip install -r requirements_dev.txt
-      echo '\n\033[93mNOTE:\033[0m \033[96mDeveloping dependencies has been installed\033[0m\n'
+      echo '\n\x1b[0;93mNOTE:\x1b[0m \x1b[0;96mDeveloping dependencies has been installed\x1b[0m\n'
       print_progress $ITER $TIMES "Installing app for developing"
       ITER=$(($ITER+1))
       # install app
@@ -106,7 +103,7 @@ if [ $INSTALL -eq 1 ]; then
       # install app
       python3 -m pip install .
     fi
-    echo '\n\033[93mNOTE:\033[0m \033[96mApp has been installed\033[0m\n'
+    echo '\n\x1b[0;93mNOTE:\x1b[0m \x1b[0;96mApp has been installed\x1b[0m\n'
     print_progress $ITER $TIMES "Checking for unet version"
     # Show unet's version
     unet --version
